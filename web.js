aa4d1a03ed5468d2ee092e3a637a9eea515a6dfe
John Smith <example@stanford.edu>
2013-07-10 23:09:48 +0000

    Updated again web.js

--- web.js
+++ web.js
@@ -2,12 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
  
-var buffer = new Buffer(fs.readFileSync('index.html'));
-
-var message = buffer.toString('utf-8');
+var buf = new Buffer(fs.readFileSync('file.html'), 'utf-8'); 
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf-8'));
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
